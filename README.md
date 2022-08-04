c738c897bfd52068281c3bbdab20e616598516c2
JohnInWI <psigncrafters@gmail.com>
2022-08-04 10:29:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-q6tnsq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-q6tnsq)
---
+++ index.js
@@ -0,0 +1,9 @@
+// // Import stylesheets
+// import './style.css';
+
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+import "./script.js";
+import "./0.setup.js";
